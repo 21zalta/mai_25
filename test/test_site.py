0beba64385b81e356dc13658ef54e4253ab5fdf7
HOME-PC\User <horsea16@gmail.com>
2025-05-05 16:16:07 +0300

    test ready

---
+++ test/test_site.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+
+import pytest
+import time
+from pages.homepage import HomePage
+from pages.produkt import ProductPage
+
+
+
+
+def test_open_s6(browser):
+
+    homepage = HomePage(browser)
+    homepage.open()
+    homepage.click_galaxy_s6()
+    product_page = ProductPage(browser)
+    product_page.check_title_is("Samsung galaxy s6")
+
+
+
+def test_two_monitors(browser):
+    homepage = HomePage(browser)
+    homepage.open()
+    homepage.click_monitor()
+    time.sleep(40)
+    homepage.check_products_count(2)
+
+
